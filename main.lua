de89e3e1f5dd356329b8d46387f3a92aafc77d60
mestow <98564343+mestow@users.noreply.github.com>
2022-03-14 19:58:11 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 --[[
 Author
 Made by TheMeMuX
-Upgraded by mestow
+Improved By mestow
 ]]
 
 
@@ -20,20 +20,22 @@ function Initialize(a_Plugin)
 	
 	LOG("Initialized " .. a_Plugin:GetName() .. " v" .. a_Plugin:GetVersion())
 	return true;
+	--TODO New recipe
 end
 
 
-function JugadorSalta(Player, OldPosition, NewPosition, PreviousIsOnGround) -- If player jump
+function JugadorSalta(Player, OldPosition, NewPosition, PreviousIsOnGround) -- If Player jump
 
 		a_Delta = OldPosition - NewPosition;
 		if (a_Delta.y < 0) then
 		if PreviousIsOnGround == true then
+			-- Player:SendMessage("Bloque: " .. Player:GetWorld():GetBlock(Vector3i(OldPosition.x-1, Player:GetPosY()-1, OldPosition.z)))	
 		if Player:GetWorld():GetBlock(Vector3i(OldPosition.x-1, OldPosition.y-1, OldPosition.z)) == E_BLOCK_IRON_BLOCK and Player:GetWorld():GetBlock(Vector3i(OldPosition.x-1, OldPosition.y-2, OldPosition.z)) == E_BLOCK_SEA_LANTERN then			
 				for i = OldPosition.y, Player:GetWorld():GetHeight(OldPosition.x-1, OldPosition.z), 1 do			
 						if Player:GetWorld():GetBlock(Vector3i(OldPosition.x-1, i, OldPosition.z)) == E_BLOCK_IRON_BLOCK and Player:GetWorld():GetBlock(Vector3i(OldPosition.x-1, OldPosition.y-2, OldPosition.z)) == E_BLOCK_SEA_LANTERN then
-
 							Player:TeleportToCoords(OldPosition.x, i+1, OldPosition.z)
-							Player:SendAboveActionBarMessage("§6UP")
+							Player:SendAboveActionBarMessage("§6Subiste")
+							Player:GetWorld():BroadcastSoundEffect("entity.witch.throw", OldPosition.x, i+1, OldPosition.z, 1, 1.3)
 							return true
 						end
 					end
@@ -43,15 +45,16 @@ function JugadorSalta(Player, OldPosition, NewPosition, PreviousIsOnGround) -- I
 	return false
 end
 
-function JugadorAgacha(Player) -- If player shift
+function JugadorAgacha(Player) -- If Player shifted
 OldPosition = Player:GetPosition()
 	if Player:GetWorld():GetBlock(Vector3i(OldPosition.x-1, OldPosition.y-1, OldPosition.z)) == E_BLOCK_IRON_BLOCK and Player:GetWorld():GetBlock(Vector3i(OldPosition.x-1, OldPosition.y-2, OldPosition.z)) == E_BLOCK_SEA_LANTERN then
 		local i = OldPosition.y-2
 		while i >= 3 do
 			if Player:GetWorld():GetBlock(Vector3i(OldPosition.x-1, i, OldPosition.z))	== E_BLOCK_IRON_BLOCK and Player:GetWorld():GetBlock(Vector3i(OldPosition.x-1, OldPosition.y-2, OldPosition.z)) == E_BLOCK_SEA_LANTERN then
 				Player:TeleportToCoords(OldPosition.x, i+1, OldPosition.z)
-				Player:SendAboveActionBarMessage("§cDown")
+				Player:SendAboveActionBarMessage("§cBajaste")
 				Player:SetInvulnerableTicks(14)
+				Player:GetWorld():BroadcastSoundEffect("entity.witch.throw", OldPosition.x, i+1, OldPosition.z, 1, 0.3)
 				return true
 			end
 			i = i - 1
